5c18f077b3c196ed0aa94a3b55316121f1306c8e
baboy <baboyzyh@gmail.com>
2016-07-25 17:35:49 +0800

    Initial Commit

---
+++ MyLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLife
+//
+//  Created by baboy on 7/25/16.
+//  Copyright © 2016 baboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLife
+//
+//  Created by baboy on 7/25/16.
+//  Copyright © 2016 baboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
